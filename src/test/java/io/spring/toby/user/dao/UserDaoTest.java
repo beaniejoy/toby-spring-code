8645473974a6ac09f6a80dd7895d04e693553d39
beaniejoy <lhb1025@gmail.com>
2021-06-08 20:13:18 +0900

    chap 2.3 : JUnit 테스팅 프레임워크
    - JUnit을 이용한 지속 가능한 단위테스트

--- src/test/java/io/spring/toby/user/dao/UserDaoTest.java
+++ src/test/java/io/spring/toby/user/dao/UserDaoTest.java
@@ -1,10 +1,11 @@
 package io.spring.toby.user.dao;
 
 import io.spring.toby.user.domain.User;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
@@ -13,27 +14,64 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDaoTest {
 
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+
+    @Before
+    public void setUp() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        dao = context.getBean("userDao", UserDao.class);
+
+        user1 = new User("beanie", "비니", "spring1");
+        user2 = new User("joy", "조이", "spring2");
+        user3 = new User("marry", "결혼", "spring3");
+    }
+
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = new User();
-        user.setId("chap1.9.2");
-        user.setName("chap1.9 테스트 과정");
-        user.setPassword("complete");
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
-        dao.add(user);
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        System.out.println(user.getId() + " 등록 성공");
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        User user2 = dao.get(user.getId());
-
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
-
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void count() throws SQLException {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFailure() throws SQLException {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.get("unknown_id");
+    }
+
+    /*public static void main(String[] args) {
         JUnitCore.main("io.spring.toby.user.dao.UserDaoTest");
-    }
+    }*/
 }
